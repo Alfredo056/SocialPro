f5c84f56fe3646581a9d45b05eb910ffc45cd9dd
Alfredo Vilca <alfredovilca374@gmail.com>
2022-12-12 14:26:27 -0500

    modulo reporte agregado

--- securityModule/autentificarUsuario/formAutenticarUsuario.php
+++ securityModule/autentificarUsuario/formAutenticarUsuario.php
@@ -1,4 +1,4 @@
-<form method='POST' action="../../model/usuario.php">
+<form method='POST' action="..\..\\model\usuario.php">
 
 <div class="mb-3">
     <label for="exampleFormControlInput1" class="form-label">Usuario</label>
